0f8756dbc8b4be6dfb62e938798fe0a7a908e24b
Artem-Drynov <144961793+Artem-Drynov@users.noreply.github.com>
2023-09-14 00:56:44 +0300

    Add files via upload

---
+++ MyHome.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    printf("   /\\   \n");
+    printf("  /  \\  \n");
+    printf(" /    \\ \n");
+    printf("/______\\\n");
+    printf("|      |\n");
+    printf("|______|\n");
+
+}
